80d47b4f14198d311bd04e70367be6f794432fcf
Syed Wyle Mustafa <wylemustafa@gmail.com>
2025-07-13 14:01:00 +0500

    add eureka discovery service

---
+++ discoveryservice/src/test/java/com/ecommerce/application/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.application.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
